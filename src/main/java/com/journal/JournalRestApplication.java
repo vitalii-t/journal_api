cbaaa43f7894fab0b3fdb0b8cbf36bbafa068f24
Vitalii <tasun.v.s@gmail.com>
2019-09-21 12:52:47 +0300

    initial commit

---
+++ src/main/java/com/journal/JournalRestApplication.java
@@ -0,0 +1,13 @@
+package com.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalRestApplication.class, args);
+    }
+
+}
