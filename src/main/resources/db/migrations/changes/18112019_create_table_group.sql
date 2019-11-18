633b674fcb11c943a755db2be1dcf1dc96d95736
mind0wner <tasun.v.s@gmail.com>
2019-11-18 23:57:16 +0200

    liquibase schema updating

--- src/main/resources/db/migrations/changes/18112019_create_table_group.sql
+++ src/main/resources/db/migrations/changes/18112019_create_table_group.sql
@@ -1,8 +1,5 @@
 create table groups
 (
     id         serial primary key not null,
-    identifier character varying(15),
-    user_id    bigint             not null
-
-    --constraint group_user_FK foreign key (user_id) references users (id)
+    identifier character varying(15)
 )
--- src/main/resources/db/migrations/changes/18112019_create_table_users.sql
+++ src/main/resources/db/migrations/changes/18112019_create_table_users.sql
@@ -1,15 +1,15 @@
 create table users
 (
     id              serial primary key not null,
+    activation_code character varying(255),
+    active          boolean,
+    email           character varying(100),
     first_name      character varying(70),
     last_name       character varying(70),
-    username        character varying(70),
-    password        character varying(70),
     list_number     bigint,
-    activation_code character varying(255),
-    email           character varying(100),
+    password        character varying(70),
+    username        character varying(70),
     group_id        bigint             not null,
-    roles           bigint             not null,
 
     constraint user_group_fk foreign key (group_id) references groups (id)
 
