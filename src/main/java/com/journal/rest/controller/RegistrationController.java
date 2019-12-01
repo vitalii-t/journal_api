e6b23edcb3afab60284c5a0d8bbf6e365af03d53
mind0wner <tasun.v.s@gmail.com>
2019-12-01 23:21:00 +0200

    updated registration response

--- src/main/java/com/journal/rest/controller/RegistrationController.java
+++ src/main/java/com/journal/rest/controller/RegistrationController.java
@@ -7,6 +7,7 @@ import com.journal.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,8 +21,8 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     @ApiOperation("Register new user")
-    public BaseResponse registration(@Valid @RequestBody CreateUserDto request) {
-        return new BaseResponse<>(userService.register(fromDto(request)), HttpStatus.CREATED.value());
+    public ResponseEntity<Boolean> registration(@Valid @RequestBody CreateUserDto request) {
+        return new ResponseEntity<>(userService.register(fromDto(request)), HttpStatus.CREATED);
     }
 
     @GetMapping("/activate/{code}")
