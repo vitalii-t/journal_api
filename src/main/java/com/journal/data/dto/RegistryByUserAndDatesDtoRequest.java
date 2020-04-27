7af874cbba86b572a34687935cde126cebb6f392
mind0wner <tasun.v.s@gmail.com>
2020-04-27 14:44:00 +0300

    added AAC to request

--- src/main/java/com/journal/data/dto/RegistryByUserAndDatesDtoRequest.java
+++ src/main/java/com/journal/data/dto/RegistryByUserAndDatesDtoRequest.java
@@ -1,5 +1,6 @@
 package com.journal.data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Data
 @Builder
+@AllArgsConstructor
 public class RegistryByUserAndDatesDtoRequest {
 
     @NotNull
