a291989da5d3a8f37dfb83779aaac2cfcbaece2f
mind0wner <tasun.v.s@gmail.com>
2020-05-25 17:57:17 +0300

    added identifier to mark each subject

--- src/main/java/com/journal/data/dto/RegistryByUserAndDatesDtoResponse.java
+++ src/main/java/com/journal/data/dto/RegistryByUserAndDatesDtoResponse.java
@@ -5,18 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 public class RegistryByUserAndDatesDtoResponse {
 
     private String subjectName;
+    private String identifier;
     private LocalDate lessonDate;
     private boolean present;
 
     public RegistryByUserAndDatesDtoResponse(Registry registry, String lang){
         subjectName = lang.equalsIgnoreCase("en") ? registry.getSubject().getSubjectNameEn() : registry.getSubject().getSubjectNameUa();
         lessonDate = registry.getDateOfLesson();
+        identifier = UUID.randomUUID().toString();
         present = registry.isPresent();
     }
 }
